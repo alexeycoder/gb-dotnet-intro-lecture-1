28e171cb6f966b90d4e49051b5133fa67ee486af
Alexey Kuznetsov <leoxander@yandex.ru>
2022-08-04 10:50:58 +0300

    Added more examples

---
+++ Ex005_ConditionIfElse/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите имя пользователя: ");
+string? userName = Console.ReadLine();
+
+if (string.IsNullOrEmpty(userName))
+{
+	Console.WriteLine("Имя не введено!");
+	return;
+}
+
+if (userName.ToLowerInvariant() == "маша")
+{
+	Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+	Console.Write("Привет, ");
+	Console.WriteLine(userName);
+}
